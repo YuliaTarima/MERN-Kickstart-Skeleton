0643bd2c166d6cc4140b2d57891bdbf3098c3e70
Yulia Tarima <yulia.tarima@gmail.com>
2020-10-13 14:34:40 -0700

    edit README.md

--- README.md
+++ README.md
@@ -12,8 +12,14 @@
 2. Install dependencies:
    - open command line in the cloned folder
    - run ```  npm install  ``` or ``` yarn ```
-3. Run the appliaction in the dev mode:
-   - open command line in the cloned folder
-   - run ```  npm run development  ``` or ``` yarn development ```
+3. Run the appliaction in the desired mode:
+   - to run in the dev mode:<br/>
+   ```  npm run development  ``` or ``` yarn development ```<br/> 
+   will get Nodemon, Webpack, and the server started for development
+   - ```  npm run build  ``` or ``` yarn build ```<br/>
+   will generate the client and server code bundles for production mode<br/> 
+   (remove the devBundle.compile code from server.js before running this script)
+   - ```  npm run start  ``` or ``` yarn start ```<br/>
+   will run the bundled code in production
 4. Open [localhost:3000](http://localhost:3000/) in the browser
 5. Start developing
--- server/server.js
+++ server/server.js
@@ -1,33 +1,43 @@
 import path from 'path'
 import express from 'express'
-import { MongoClient } from 'mongodb'
+import {MongoClient} from 'mongodb'
 import template from './../template'
-//comment out before building for production
-import devBundle from './devBundle'
+import devBundle from './devBundle' // dev mode only, comment before prod build
+// Webpack will compile and bundle the React code into dist/bundle.js.
 
 const app = express()
-//comment out before building for production
-devBundle.compile(app)
+devBundle.compile(app) // dev mode only, comment out before building for production
 
 const CURRENT_WORKING_DIR = process.cwd()
+// Webpack will compile client-side code in both development and production mode,
+// then place the bundled files in the dist folder.
+// To make these static files available on requests from the client side:
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.get('/', (req, res) => {
-  res.status(200).send(template())
+    // When the server receives a request at the root URL /,
+    // we will render template.js in the browser.
+    res.status(200).send(template())
 })
 
 let port = process.env.PORT || 3000
+// listen on the specified port for incoming requests
 app.listen(port, function onStart(err) {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', port)
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', port)
 })
 
-// Database Connection URL
+// connect Node server to MongoDB
+// make sure you have MongoDB running
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
-// Use connect method to connect to the server
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },(err, db)=>{
-  console.log("Connected successfully to mongodb server")
-  db.close()
+// MongoClient is the driver that connects to the running MongoDB instance
+// using its url and allows us to implement the database related code in the backend.
+MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, db) => {
+    console.log("Connected successfully to mongodb server")
+    db.close()
 })
